139bef6192234efba796c3ea61e71318ffe489eb
AryaNaufal <arya.naufal.andt@gmail.com>
2024-05-30 07:20:34 +0700

    fix(deploy): image unoptimized

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: 'icon.icepanel.io'
