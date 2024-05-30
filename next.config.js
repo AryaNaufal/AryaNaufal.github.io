ab77cbade7964bb37fbdef741d546a99d84e45f2
Nastavnic <79558550+AryaNaufal@users.noreply.github.com>
2024-05-30 14:44:04 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
   output: "export",
->>>>>>> cddcd5c86e46379b7196b2d40ce864bd03a56628
   reactStrictMode: true,
   swcMinify: true,
   images: {
