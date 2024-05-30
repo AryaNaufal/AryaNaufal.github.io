d387f0f8227468c4318037de1888f7c26b9f4d97
AryaNaufal <79558550+AryaNaufal@users.noreply.github.com>
2024-05-30 09:14:26 +0000

    Deploying to gh-pages from @ AryaNaufal/AryaNaufal.github.io@dbad9387d8f84e078e8afc76069395ea69efe7c8 🚀

---
+++ _next/static/chunks/pages/Components/navbar-cf3629ba9cbe6609.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[880],{6530:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Components/navbar",function(){return u(4493)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=6530)}),_N_E=n.O()}]);
